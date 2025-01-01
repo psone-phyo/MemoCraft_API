26e24b4d55f1a51b3e37feeea0bc5d3af5ba3fa6
psone_phyo <psone9921@gmail.com>
2025-01-01 12:45:45 +0630

    finished with login and register api including validation

--- index.js
+++ index.js
@@ -9,45 +9,19 @@ const express = require('express');
 const cors = require('cors');   
 const app = express();
 
-const jwt = require("jsonwebtoken");
+
 const { authenticateToken} = require("./utils")
 
-const {query, matchedData, validationResult} = require("express-validator");
+const {query,body, matchedData, validationResult} = require("express-validator");
+const routes = require('./routes/Authentication');
+
 app.use(express.json());
 
 app.use(cors(
     { origin: "*" }
 ));
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World' });   
-});
-
-app.post('/create-user', async (req, res)=>{
-    const {username,email,password} = req.body;
-    if (!username || !email || !password) return res.status(401).json({ error: 'Missing required fields1' });
-
-    const isUser = await User.findOne({email: email});
-
-    if(isUser) return res.status(401).json({ error: 'Missing required fields2' });
-
-    const user = new User({
-        username: username,
-        email: email,
-        password: password,
-    });
-
-    await user.save();
-
-    const Token = jwt.sign({user}, process.env.ACCESS_KEY_SECRET, {expiresIn: "1h"});
-
-    return res.status(201).json(
-        {
-            data: user,
-            Token: Token
-        }
-    )
-})
+app.use('/api/', routes);
 
 app.get('/users', async(req, res) => {
     const users = await User.find();
---
+++ routes/Authentication.js
@@ -0,0 +1,71 @@
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const {body, matchedData, validationResult} = require("express-validator");
+const User= require('../models/User.model')
+
+router.post('/create-user', body('username').notEmpty().withMessage('Username is required')
+                        ,body('email').notEmpty().withMessage('Email is required').isEmail().withMessage('Email is not valid').custom(async (email)=>{
+                            const takenEmail = await User.findOne({email: email});
+                            if (takenEmail){
+                                throw new Error('Email already exists');
+                            }
+                        })
+                        ,body('password').notEmpty().withMessage('Password is required').isLength({ min: 8 }).withMessage('Password must be at least 8 characters')
+                        , async(req,res)=>{
+                            const errors = validationResult(req);
+                            if (!errors.isEmpty()){
+                                return res.status(400).json({ errors: errors.array().map((e)=>e.msg) });
+                            }
+
+                            const user = new User(matchedData(req));
+
+                            await user.save();
+
+                            const Token = jwt.sign({user}, process.env.ACCESS_KEY_SECRET, {expiresIn: '1h'})
+
+                            return res.status(201).json({
+                                data: user,
+                                Token: Token
+                            })
+                        }
+)
+
+router.post('/login', 
+    body('email').notEmpty().withMessage('Email is required')
+    ,body('password')
+    .custom(async(password)=>{
+        const hasPsw = await User.findOne({password: password});
+        if (!hasPsw){
+            throw new Error('Incorrect Password');
+        }
+    })
+    ,async(req, res) => {
+        const errors = validationResult(req);
+        const requiredEmail = errors.array().filter((e)=>{
+            if (e.msg === "Email is required"){
+                return e;
+            }
+        })
+
+        if (requiredEmail.length > 0){
+            return res.status(400).json({errors: "Email is required"});
+        }
+        
+        const checkEmail = await User.findOne({email: req.body.email});
+        if (!checkEmail){
+            return res.status(400).json({errors: "Email does not Exist"})
+        }
+
+        if (!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array().map((e)=>e.msg) });
+        }
+
+        const user = await User.findOne({email: req.body.email});
+        const Token = jwt.sign({user}, process.env.ACCESS_KEY_SECRET, {expiresIn: '1h'});
+        return res.status(200).json({
+            data: user,
+            Token: Token
+        })
+})
+
+module.exports = router;
